8afe0f8fce05b812f57b794e1f5c70f84fdccf86
Harisson Nascimento <harisson.nas@gmail.com>
2021-03-09 11:02:50 -0300

    feat: initializing project

---
+++ src/main/java/br/com/zup/propostazup/PropostaZupApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.propostazup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaZupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaZupApplication.class, args);
+	}
+
+}
